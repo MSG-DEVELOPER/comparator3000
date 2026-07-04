26eb1dd987357f621d2515748931820e7936c3a1
Joker <puebloseko@gmail.com>
2026-07-04 12:59:26 +0200

    .map to info from ddbb

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -3,11 +3,31 @@
 
 import { query } from "@/lib/db";
 
+interface productoType{
+  name:string,
+  id:number
+}
+
 async function Dashboard() {
   const data = await query("SELECT * from products");
   const productos = data.rows;
+  console.log(productos[0].name);
+  console.log(productos[0]);
+  console.log( productos);
 
-  return <div>{productos[1].name}</div>;
+
+  return <div>
+   
+    <div>
+lista productos
+{productos.map((item:productoType)=>(<p key={item.id}>{item.name}</p>))}
+
+
+    </div>
+
+
+
+  </div>;
 }
 
 export default Dashboard;
